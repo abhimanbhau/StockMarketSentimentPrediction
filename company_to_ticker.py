0c7878f4c76a2d2ca372c6e4350cfb47b8e95de8
Abhiman Kolte <akolte@icloud.com>
2020-06-18 22:52:19 -0500

    code cleanup

--- company_to_ticker.py
+++ company_to_ticker.py
@@ -5,44 +5,50 @@ import urllib
 
 # Enter your AlphaVantage API Key here
 
-API_KEY = ''
+API_KEY = ""
 
-client = boto3.client('dynamodb')
+client = boto3.client("dynamodb")
+
+cors_headers = {
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+}
+
 
 def lambda_handler(event, context):
-    
-    query = urllib.parse.quote(event['queryStringParameters']['query'])
-   
-    response = client.get_item(TableName='stockmarket_symbols', Key={'name':{'S':query.upper()}})
+
+    query = urllib.parse.quote(event["queryStringParameters"]["query"])
+
+    response = client.get_item(
+        TableName="stockmarket_symbols", Key={"name": {"S": query.upper()}}
+    )
     symbol = ""
     if len(response) == 1:
-        url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={}&apikey={}".format(query, API_KEY)
+        url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={}&apikey={}".format(
+            query, API_KEY
+        )
         f = urllib.request.urlopen(url)
         result = json.loads(f.read().decode("utf-8"))
-        symbols = result['bestMatches']
+        symbols = result["bestMatches"]
         if len(symbols) == 0:
             return {
-                'statusCode': 200,
-                'headers': {
-                    "Access-Control-Allow-Headers" : "Content-Type",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                },
-                'body': json.dumps("NULL")
+                "statusCode": 200,
+                "headers": cors_headers,
+                "body": json.dumps("NULL"),
             }
-        symbol = symbols[0]['1. symbol']
-        client.put_item(TableName='stockmarket_symbols', Item={'name':{'S':query.upper()},'symbol':{'S':symbol}})
+        symbol = symbols[0]["1. symbol"]
+        client.put_item(
+            TableName="stockmarket_symbols",
+            Item={"name": {"S": query.upper()}, "symbol": {"S": symbol}},
+        )
     else:
-        #print(response['Item']['symbol'])
-        symbol = response['Item']['symbol']['S']
-    
+        # print(response['Item']['symbol'])
+        symbol = response["Item"]["symbol"]["S"]
+
     # TODO implement
     return {
-        'statusCode': 200,
-        'headers': {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        'body': json.dumps(symbol)
+        "statusCode": 200,
+        "headers": cors_headers,
+        "body": json.dumps(symbol),
     }
--- stock_data_by_ticker.py
+++ stock_data_by_ticker.py
@@ -3,53 +3,40 @@ import urllib.request
 
 # Enter your AlphaVantage API Key here
 
-API_KEY = ''
+API_KEY = ""
+
+# Headers for CORS support
+
+cors_headers = {
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+}
+
 
 def lambda_handler(event, context):
-    symbol = event['queryStringParameters']['symbol']
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}".format(symbol, API_KEY)
+    symbol = event["queryStringParameters"]["symbol"]
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}".format(
+        symbol, API_KEY
+    )
     f = urllib.request.urlopen(url)
     print(url)
     try:
         result = json.loads(f.read().decode("utf-8"))
         print((result))
-        if result['Time Series (Daily)'] is None:
+        if result["Time Series (Daily)"] is None:
             return {
-                'statusCode': 200,
-                'headers': {
-                    "Access-Control-Allow-Headers" : "Content-Type",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                },
-                'body': json.dumps('ERROR')
+                "statusCode": 200,
+                "headers": cors_headers,
+                "body": json.dumps("ERROR"),
             }
-        print(('aaaaa'))
         res = []
-        for d in result['Time Series (Daily)'].items():
-            #res['{}'.format(d[0])] = d[1]['4. close']
-            obj = {
-                "x" : d[0],
-                "y" : d[1]['4. close']
-            }
+        for d in result["Time Series (Daily)"].items():
+            # res['{}'.format(d[0])] = d[1]['4. close']
+            obj = {"x": d[0], "y": d[1]["4. close"]}
             res.append(obj)
     except Exception as e:
-        return {
-        'statusCode': 500,
-        'headers': {
-                    "Access-Control-Allow-Headers" : "Content-Type",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-                },
-        'body': json.dumps(str(e))
-        }
+        return {"statusCode": 500, "headers": cors_headers, "body": json.dumps(str(e))}
     res = res[::-1]
     # TODO implement
-    return {
-        'statusCode': 200,
-        'headers': {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        'body': json.dumps(res[-30:])
-    }
+    return {"statusCode": 200, "headers": cors_headers, "body": json.dumps(res[-30:])}
